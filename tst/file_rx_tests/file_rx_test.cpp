fc6d8e266a4964882b1378a5849bc4097e33108b
MilkyTea123 <tsienmd@gmail.com>
2025-04-23 19:29:02 -0700

    updated rx file

--- tst/file_rx_tests/file_rx_test.cpp
+++ tst/file_rx_tests/file_rx_test.cpp
@@ -2,7 +2,7 @@
 
 AudioTransmitter audioTx(AudioProfile(1000.0, {63000, 67000}, 50000));
 
-string result;
+string result, rx_data;
 uint8_t headerByte = 0x00;
 uint8_t err;
 
@@ -10,12 +10,24 @@ int main()
 {
     audioTx.init_stream();
     init_receiver();
-    cout << "stream initialized" << endl;
+    // cout << "stream initialized" << endl;
     while (headerByte != DATA_DONE)
     {
         err = listen(result);
         usleep(1000);
-        transmit_data(audioTx, CTRL_MODE, ACK);
+        getHeaderByte(result, headerByte);
+        get_packet_data(result, rx_data);
+
+        if (check_received_crc(result))
+        {
+            transmit_data(audioTx, CTRL_MODE, ACK);
+            cout << rx_data << endl;
+        }
+        else
+        {
+            transmit_data(audioTx, CTRL_MODE, NAK_SEND);
+            cout << "bad attempt" << endl;
+        }
     }
     audioTx.close_stream();
     close_receiver();
