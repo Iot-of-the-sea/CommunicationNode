4dea15b8f312bc97d748448b3b9b752a2c80a907
MilkyTea123 <tsienmd@gmail.com>
2025-04-28 18:05:25 -0700

    receive testing

--- tst/file_rx_tests/file_rx_test.cpp
+++ tst/file_rx_tests/file_rx_test.cpp
@@ -1,11 +1,13 @@
 #include "../../lib/audio/audiotransmitter.h"
+#include <fstream>
 
 AudioTransmitter audioTx(AudioProfile(1000.0, {63000, 67000}, 50000));
 
 TimeoutHandler timeout(1000000);
 
-string result, rx_data;
+string result, last_rx_data, rx_data;
 uint8_t headerByte = 0x00;
+uint8_t lastHeader = 0x00;
 uint8_t err;
 
 int main()
@@ -13,29 +15,39 @@ int main()
     cout << "running file Rx tests" << endl;
     audioTx.init_stream();
     init_receiver();
-    while (headerByte != DATA_DONE)
+    FileWriter file("./tst/testFile.txt");
+    file.open();
+    int counter = 0;
+    while (headerByte != DATA_DONE && counter < 20)
     {
         err = listen(result, &timeout);
         if (!err && check_received_crc(result))
         {
             getHeaderByte(result, headerByte);
             get_packet_data(result, rx_data);
+
+            if (headerByte != lastHeader)
+            {
+                file.write(last_rx_data);
+            }
+
+            last_rx_data = rx_data;
+            lastHeader = headerByte;
             transmit_data(audioTx, CTRL_MODE, ACK);
-            cout << "data: " << rx_data << endl;
+            counter = 0;
         }
         else
         {
             transmit_data(audioTx, CTRL_MODE, NAK_SEND);
-            // cout << "bad attempt" << endl;
+            counter++;
         }
     }
-    transmit_data(audioTx, CTRL_MODE, ACK);
 
-    cout << "not segfaulted 1" << endl;
     audioTx.close_stream();
     close_receiver();
+    file.close();
 
-    cout << "not segfaulted 2" << endl;
+    cout << "close" << endl;
 
     return 0;
 }
